07dab209be98432556345f179fa1441926ebbe19
ajchan11 <ajchan11@gmail.com>
2015-10-14 13:57:43 -0700

    adds login stuff

--- index.php
+++ index.php
@@ -31,11 +31,63 @@ footer {
 	<div class="row pages">
 		<div class="col-sm-2"></div>
 		<div class="col-sm-4 text-center animated fadeInLeft">
-				<h3> Your Work, Your Way</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum rhoncus consectetur. Morbi ante nunc, scelerisque at ipsum quis, maximus mattis urna. Etiam cursus euismod eros. Nunc eget nulla et felis rhoncus pulvinar. Maecenas iaculis et augue id elementum. Ut sed pharetra diam. Sed condimentum ultricies neque, nec placerat ligula euismod ut.</p>
+				<h3 class="text-center"> Your Work, Your Way</h3>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum rhoncus consectetur. Morbi ante nunc, scelerisque at ipsum quis, maximus mattis urna. Etiam cursus euismod eros. Nunc eget nulla et felis rhoncus pulvinar. Maecenas iaculis et augue id elementum. Ut sed pharetra diam. Sed condimentum ultricies neque, nec placerat ligula euismod ut.</p>
 		</div>
 		<div class="col-sm-4 text-center animated fadeInRight">
-			Insert login here
+			<h4 class="text-center">Login to UpKeep</h4>
+			<div class="login-body mt-30">
+          <form name="loginForm" ng-submit="login();">
+            <div class="row">
+            	<div class="col-xs-2"></div>
+              <div class="col-xs-8">
+                <div class="form-group">
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" class="form-control" id="username" placeholder="Username" name="username" ng-model="login.username" value="" required>
+                  </div>
+              	</div>
+              </div>
+              <div class="col-xs-2"></div>
+            </div>
+            <div class="row">
+            	<div class="col-xs-2"></div>
+              <div class="col-xs-8">
+                <div class="form-group">
+                  <div class="input-group">
+                    <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    <input type="password" class="form-control" id="password" placeholder="Password" name="password" ng-model="login.password" value="" required>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-2"></div>
+            </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 ">
+              	<br/>
+                <a class="text-info" href="#" ui-sref="login.reset-password">Reset your password?</a>
+              </div>
+            </div>
+            <div class="row mt-30">
+              <div class="col-xs-12">
+              	<br/>
+                <button class="btn" ng-class="{'btn-grey':loading, 'btn-turquoise':!loading}">[[loading?'Logging you in..': 'LOGIN']]
+                </button><span class="spin loading" ng-if="loading"></span>
+              </div>
+            </div>
+          </form>
+            <div class="hr mt-20"></div>
+            <div class="row mt-30">
+              <div class="col-xs-12">
+              	<br/>
+                <a class="btn btn-primary" ng-click="useAsGuest();">Use as Guest</a>
+
+                <span class="btn btn-info" ui-sref-opts="{ reload: true }" ng-class="{'hidden':settings.useAsGuestLoading}" ui-sref="login.signup">Free Sign Up</span>
+                <span ng-if="settings.useAsGuestLoading" class="spin loading"></span>
+              </div>
+            </div>
+        </div>
+      </div>
 		</div>
 		<div class="col-sm-2"></div>
 	</div>
@@ -45,18 +97,18 @@ footer {
 		<div class="col-sm-4 text-center">
 			<img src="img/sec1-doodle.png" alt="section1" class="text-center img-sec1"/>
 		</div>
-		<div class="col-sm-4 text-center">
-			<h4> Manage your assets and work orders at the tip of your fingers</h4>
-			<p> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
+		<div class="col-sm-4 text-sec1">
+			<h4 class="text-center"> Manage your assets and work orders at the tip of your fingers</h4>
+			<p class="text-left"> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
 		</div>
 		<div class="col-sm-2"></div>
 	</div>
 
-	<div class="row pages ">
+	<div class="row pages">
 		<div class="col-sm-2"></div>
-		<div class="col-sm-4 text-center">
-			<h4> Work together with your entire team</h4>
-			<p> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
+		<div class="col-sm-4 text-sec2">
+			<h4 class="text-center"> Work together with your entire team</h4>
+			<p class="text-left"> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
 		</div>
 		<div class="col-sm-4 text-center">
 			<img src="img/sec2-doodle.png" alt="section2" class="text-center img-sec2"/>
@@ -69,9 +121,9 @@ footer {
 		<div class="col-sm-4 text-center">
 			<img src="img/sec3-doodle.png" alt="section3" class="text-center img-sec3"/>
 		</div>
-		<div class="col-sm-4 text-center">
-			<h4> Keep your Data</h4>
-			<p> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
+		<div class="col-sm-4 text-sec3">
+			<h4 class="text-center"> Keep your Data</h4>
+			<p class="text-left"> Mauris malesuada vehicula commodo. Nulla vel volutpat massa. Donec ut elementum augue. Praesent dapibus ornare risus eget placerat. Donec id purus ut arcu molestie iaculis ac rutrum sem. Praesent vehicula sem eu tincidunt pulvinar. Praesent diam lorem, aliquet et turpis vitae, efficitur mollis justo.</p>
 		</div>
 		<div class="col-sm-2"></div>
 	</div>
---
+++ js/plugins.js
@@ -0,0 +1,189 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+ new WOW().init();
+
+
+//Sarpise slider
+
+			jQuery(function($){
+				
+				$.supersized({
+				
+					// Functionality
+					slideshow               :   1,			// Slideshow on/off
+					autoplay				:	1,			// Slideshow starts playing automatically
+					start_slide             :   1,			// Start slide (0 is random)
+					stop_loop				:	0,			// Pauses slideshow on last slide
+					random					: 	0,			// Randomize slide order (Ignores start slide)
+					slide_interval          :   3000,		// Length between transitions
+					transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+					transition_speed		:	1000,		// Speed of transition
+					new_window				:	1,			// Image links open in new window/tab
+					pause_hover             :   0,			// Pause slideshow on hover
+					keyboard_nav            :   1,			// Keyboard navigation on/off
+					performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+					image_protect			:	1,			// Disables image dragging and right click with Javascript
+															   
+					// Size & Position						   
+					min_width		        :   0,			// Min width allowed (in pixels)
+					min_height		        :   0,			// Min height allowed (in pixels)
+					vertical_center         :   1,			// Vertically center background
+					horizontal_center       :   1,			// Horizontally center background
+					fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
+					fit_portrait         	:   1,			// Portrait images will not exceed browser height
+					fit_landscape			:   0,			// Landscape images will not exceed browser width
+															   
+					// Components							
+					slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+					thumb_links				:	1,			// Individual thumb links for each slide
+					thumbnail_navigation    :   0,			// Thumbnail navigation
+					slides 					:  	[			// Slideshow Images
+														{image : 'img/2.jpg'},
+														{image : 'img/3.jpg'},
+														{image : 'img/4.jpg'}
+													
+													
+														
+												],
+												
+					// Theme Options			   
+					progress_bar			:	1,			// Timer for each slide							
+					mouse_scrub				:	0
+					
+				});
+		    });
+		    
+$("body").css("overflow-x","hidden");
+
+
+
+
+
+
+
+
+
+
+
+
+//Contact from Script
+$(document).ready(function() {
+    $("#submit_btn").click(function() { 
+        //get input field values
+        var user_name       = $('input[name=name]').val(); 
+        var user_email      = $('input[name=email]').val();
+        var user_message    = $('textarea[name=message]').val();
+        
+        //simple validation at client's end
+        //we simply change border color to red if empty field using .css()
+        var proceed = true;
+        if(user_name==""){ 
+            $('input[name=name]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(user_email==""){ 
+            $('input[name=email]').css('border-color','red'); 
+            proceed = false;
+        }
+       
+        if(user_message=="") {  
+            $('textarea[name=message]').css('border-color','red'); 
+            proceed = false;
+        }
+
+        //everything looks good! proceed...
+        if(proceed) 
+        {
+            //data to be sent to server
+            post_data = {'userName':user_name, 'userEmail':user_email, 'userMessage':user_message};
+            
+            //Ajax post data to server
+            $.post('getintouch.php', post_data, function(response){  
+
+                //load json data from server and output message     
+				if(response.type == 'error')
+				{
+					output = '<div class="error">'+response.text+'</div>';
+				}else{
+				    output = '<div class="success">'+response.text+'</div>';
+					
+					//reset values in all input fields
+					$('#contact_form input').val(''); 
+					$('#contact_form textarea').val(''); 
+				}
+				
+				$("#result").hide().html(output).slideDown();
+            }, 'json');
+			
+        }
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form input, #contact_form textarea").keyup(function() { 
+        $("#contact_form input, #contact_form textarea").css('border-color',''); 
+        $("#result").slideUp();
+    });
+    
+});
+
+
+(function($) {
+    var $window = $(window),
+        $cba = $('.navbar-default');
+		 $cle = $('.navbar-right');
+		 
+		if ($(window).width() < 767) {
+		 
+		    $cba.removeClass('cbp-af-header');
+			 $cle.removeClass('cl-effect-13');
+			
+		}
+		else {
+		
+		    $cba.addClass('cbp-af-header');
+			 $cle.addClass('cl-effect-13');
+			
+		}
+
+   /* $window.resize(function resize() {
+        if ($window.width() < 767) {
+           $cba.removeClass('cbp-af-header');
+        }
+		else {
+		alert('More than 960');
+}
+        
+    });*/
+})(jQuery);
+
+//Affix Bootstrap
+$(document).ready(function(){
+    $("#tutonav").affix({
+        offset: { 
+            top: 100
+     	}
+    });
+});
